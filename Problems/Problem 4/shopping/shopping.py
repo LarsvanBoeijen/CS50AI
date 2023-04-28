b0fe747fe524cb2782c3974677ffdb0cd38546c3
LarsvanBoeijen <lars.age@gmail.com>
2023-04-28 22:56:11 +0000

    Completed Shopping problem

--- Problems/Problem 4/shopping/shopping.py
+++ Problems/Problem 4/shopping/shopping.py
@@ -5,6 +5,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 
 TEST_SIZE = 0.4
+MONTHS = ["Jan", "Feb", "Mar", "Apr", "May", "June", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+VISITOR_TYPES = ["New_Visitor", "Returning_Visitor"]
+BOOLEAN = ["FALSE", "TRUE"]
 
 
 def main():
@@ -22,6 +25,7 @@ def main():
     # Train model and make predictions
     model = train_model(X_train, y_train)
     predictions = model.predict(X_test)
+    
     sensitivity, specificity = evaluate(y_test, predictions)
 
     # Print results
@@ -59,15 +63,60 @@ def load_data(filename):
     labels should be the corresponding list of labels, where each label
     is 1 if Revenue is true, and 0 otherwise.
     """
-    raise NotImplementedError
-
+    # Create lists for evidence and labels
+    evidence = []
+    labels = []
+    
+    with open(filename) as f:
+        # Create csv Reader object
+        reader = csv.reader(f)
+        
+        # Skip over header line
+        next(reader)
+        
+        # For every row in the dataset
+        for row in reader:
+            # Cast the data points to their respective data type
+            for column, dataPoint in enumerate(row):
+                # Administrative, Informational, ProductRelated, Month, OperatingSystems, Browser, Region and TrafficType
+                # can all directly be cast as an integer
+                if column in [0, 2, 4, 11, 12, 13, 14]:
+                    row[column] = int(dataPoint)
+                # Turn the month string into a numerical value
+                elif column == 10:
+                    row[column] = MONTHS.index(dataPoint)
+                # Turn VisitorType string into a numerical value: Returning_Visitor = 1, New_Visitor/Other = 0
+                elif column == 15:
+                    if dataPoint == "Returning_Visitor":
+                        row[column] = 1
+                    else:
+                        row[column] = 0
+                # Turn the Weekend and Revenue strings into a numerical value
+                elif column in [16, 17]:
+                    row[column] = BOOLEAN.index(dataPoint)
+                else:
+                    row[column] = float(dataPoint)
+                    
+            # Add everything but the last element to the evidence list
+            evidence.append(row[:-1])
+            
+            # Add the last element to the labels list
+            labels.append(row[-1])
+    
+    return evidence, labels
 
 def train_model(evidence, labels):
     """
     Given a list of evidence lists and a list of labels, return a
     fitted k-nearest neighbor model (k=1) trained on the data.
     """
-    raise NotImplementedError
+    # Generate the k-nearest neigbor model
+    model = KNeighborsClassifier(n_neighbors=1)
+    
+    # Train model on training set
+    model.fit(evidence, labels)
+    
+    return model
 
 
 def evaluate(labels, predictions):
@@ -85,7 +134,29 @@ def evaluate(labels, predictions):
     representing the "true negative rate": the proportion of
     actual negative labels that were accurately identified.
     """
-    raise NotImplementedError
+    # Set up the counts for the number of positive/negative labels as well as how often those labels were correctly identified
+    nPositive = 0
+    nPositiveCorrect = 0
+    nNegative = 0
+    nNegativeCorrect = 0
+    
+    for label, prediction in zip(labels, predictions):
+        if label == 0:
+            nNegative += 1
+            if label == prediction:
+                nNegativeCorrect += 1 
+        elif label == 1:
+            nPositive += 1
+            if label == prediction:
+                nPositiveCorrect += 1 
+    
+    # Calculate sensitivity
+    sensitivity = nPositiveCorrect / nPositive
+    
+    # Calculate specificity
+    specificity = nNegativeCorrect / nNegative
+    
+    return sensitivity, specificity
 
 
 if __name__ == "__main__":
